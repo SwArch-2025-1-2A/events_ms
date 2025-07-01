1ac4fea2eef0141de122ff496c816b07d4d26e3d
arojasag <arojasag@unal.edu.co>
2025-06-30 23:03:56 -0500

    Use environmental variables to configure the database connection string

--- NetApiPostgreSQL/Program.cs
+++ NetApiPostgreSQL/Program.cs
@@ -9,8 +9,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //PostgreSQL Config
-
-var postgresConfig = new PostgresSQL(builder.Configuration["PostgresSQL"]);
+var dbHost = builder.Configuration["DB_HOSTNAME"];
+var dbPort = builder.Configuration["DB_PORT"];
+var dbName = builder.Configuration["DB_NAME"];
+var dbUser = builder.Configuration["DB_USER"];
+var dbPassword = builder.Configuration["DB_PASSWORD"];
+var connectionString = $"Host={dbHost};" + $"Port={dbPort};" + $"Database={dbName};" + $"Username={dbUser};" + $"Password={dbPassword};";
+var postgresConfig = new PostgresSQL(connectionString);
 
 builder.Services.AddSingleton(postgresConfig);
 builder.Services.AddScoped<EventosInterface, Eventos>();
